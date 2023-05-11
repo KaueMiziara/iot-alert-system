4356a350c6f0afcc8857bad4b25c5570dfc6ba96
KaueMiziara <kauemiziara@gmail.com>
2023-05-11 15:33:48 -0300

    Initialized components && Defined pins

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,33 @@ fn main() -> ! {
     wdt0.disable();
     wdt1.disable();
 
-    println!("Hello world!");
+    // Initialize Delay
+    let mut delay = Delay::new(&clocks);
+
+    // Initialize IO && Pin definitions
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let (mut internal_led, mut buzzer, sda, scl) = (
+        io.pins.gpio2.into_push_pull_output(),
+        io.pins.gpio33.into_push_pull_output(),
+        io.pins.gpio21,
+        io.pins.gpio22,
+    );
+
+    // Configure I2C
+    let i2c = i2c::I2C::new(
+        peripherals.I2C0,
+        sda,
+        scl,
+        100u32.kHz(),
+        &mut system.peripheral_clock_control,
+        &clocks,
+    );
+    delay.delay_ms(255u8);
+
+    // Initialize MPU6050 module
+    let mut mpu = Mpu6050::new(i2c);
+    mpu.init(&mut delay)
+        .expect("Error while initializing MPU6050");
 
     loop {}
 }
